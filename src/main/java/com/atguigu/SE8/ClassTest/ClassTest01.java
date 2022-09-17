6e3981389f6dd1f443cae8186c1d64e2c74a73ba
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-17 09:59:31 +0800

    AM 01st commit

---
+++ src/main/java/com/atguigu/SE8/ClassTest/ClassTest01.java
@@ -0,0 +1,29 @@
+package com.atguigu.SE8.ClassTest;
+
+/**
+ * Description==>TODO
+ * BelongsProject==>_01_JavaSE
+ * BelongsPackage==>com.atguigu.SE8.ClassTest
+ * CreateTime==>2022-09-17 09:57:11
+ * Version==>1.0
+ * Author==>02雪乃赤瞳楪祈校条祭制作委员会 wyq_start
+ */
+public class ClassTest01 {
+
+    public class classTest01 {
+    }
+
+    class classTest02 {
+    }
+
+    protected class classTest03 {
+    }
+
+    private class classTest04 {
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
