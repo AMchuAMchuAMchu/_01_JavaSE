83e9686483473e9792133f0cb2bb757b251e6d8c
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-03 09:05:51 +0800

    AM 01st commit

---
+++ src/main/java/com/atguigu/SE8/StringTest/StringTrimTest.java
@@ -0,0 +1,34 @@
+package com.atguigu.SE8.StringTest;
+
+import jdk.internal.cmm.SystemResourcePressureImpl;
+import org.junit.Test;
+
+import java.util.Scanner;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> _01_JavaSE
+ * BelongsPackage ==> com.atguigu.SE8.StringTest
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-03 09:03:00
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+public class StringTrimTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("请输入...🍹🍹");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        System.out.println("客观您输入的长度是 >> "+input.length());
+
+        String result = input.trim();
+
+        System.out.println("处理之后的真实长度 >> "+result);
+
+    }
+
+}
